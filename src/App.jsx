c098a9721ef639234c2620a4724b4c67df4105e2
sileo18 <lucastoledo358@gmail.com>
2025-12-26 13:40:09 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -124,7 +124,7 @@ function App() {
             </a>
           </div>
 
-          <div className="max-w-6xl mx-auto">
+          <div className="max-w-6xl mx-auto py-10">
             <h3 className="text-2xl font-bold mb-8 text-center">Videoclipe em Destaque</h3>
             <div className="grid md:grid-cols-2 gap-8 items-center">
               <a
